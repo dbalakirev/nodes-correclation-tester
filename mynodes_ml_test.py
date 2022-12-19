3cf8de361f7de6934db3fdf92bc920e2f4f8a592
Dávid Balakirev <david.balakirev@adnovum.hu>
2022-12-19 17:35:38 +0100

    Added more train data and kept one (D) for test data

--- mynodes_ml_test.py
+++ mynodes_ml_test.py
@@ -12,7 +12,15 @@ class TestMyNodesML(unittest.TestCase):
             mynodes = MyNodesML()
             x = [row[1:-1]]
             result = mynodes.predict(x)
-            self.assertEqual(expected, result, "{} did not work!".format(code))
+            self.assertEqual(expected, result, "Code {} did not work!".format(code))
+
+    # D is not added to the dataset yet
+    def test_predict_unseen_record(self):
+        x = [[4,8,2,2,4,3]]
+        expected = 0
+        mynodes = MyNodesML()
+        result = mynodes.predict(x)
+        self.assertEqual(expected, result, "Example D {} did not work!".format(x))
 
 if __name__ == '__main__':
     unittest.main()
