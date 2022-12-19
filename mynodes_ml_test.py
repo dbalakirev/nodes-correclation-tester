b314209af76943e5727b9796f56891d016d67bc3
Dávid Balakirev <david.balakirev@adnovum.hu>
2022-12-19 14:01:32 +0100

    Implemented a test to benchmark against trained records

--- mynodes_ml_test.py
+++ mynodes_ml_test.py
@@ -1,25 +1,18 @@
 import unittest
+import pandas as pd
 from mynodes_ml import MyNodesML
 
 class TestMyNodesML(unittest.TestCase):
 
-    def test_A(self):
-        mynodes = MyNodesML()
-        expected = 1
-        result = mynodes.predict([[4,8,2,2,4,2]])
-        self.assertEqual(expected, result)
-
-    def test_B(self):
-        mynodes = MyNodesML()
-        expected = 1
-        result = mynodes.predict([[4,8,3,1,4,2]])
-        self.assertEqual(expected, result)
-
-    def test_C(self):
-        mynodes = MyNodesML()
-        expected = 0
-        result = mynodes.predict([[4,6,3,1,2,2]])
-        self.assertEqual(expected, result)
+    def test_predict_all_trained_records(self):
+        dataset = pd.read_csv("data.csv")
+        for index, row in dataset.iterrows():
+            code = row[0]
+            expected = row[-1]
+            mynodes = MyNodesML()
+            x = [row[1:-1]]
+            result = mynodes.predict(x)
+            self.assertEqual(expected, result, "{} did not work!".format(code))
 
 if __name__ == '__main__':
     unittest.main()
